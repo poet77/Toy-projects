6c835cd13042cfb51d285b2072319fe70b250c65
poet77 <ambitious_777@mail.ustc.edu.cn>
2025-01-18 20:56:22 +0800

    relationdatabase

---
+++ freecodecamp/Relational_database/project2/insert_data.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Clear existing data
+$PSQL "TRUNCATE TABLE games, teams RESTART IDENTITY"
+
+# Read the CSV file
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  # Skip header
+  if [[ $YEAR != "year" ]]
+  then
+    # Insert unique teams
+    for TEAM in "$WINNER" "$OPPONENT"
+    do
+      TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$TEAM'")
+      if [[ -z $TEAM_ID ]]
+      then
+        INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$TEAM')")
+      fi
+    done
+
+    # Get team IDs
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
+    # Insert game
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+  fi
+done
